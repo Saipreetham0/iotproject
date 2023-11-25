32d533fbb092cadc5cf102c8f37c9b00240e2058
Saipreetham <39019648+Saipreetham0@users.noreply.github.com>
2023-11-25 16:30:45 +0530

    Dashboard Update

--- app/components/SensorDataTable/index.js
+++ app/components/SensorDataTable/index.js
@@ -1,107 +1,23 @@
 // SensorDataTable.js
 import React, { useState, useEffect } from "react";
 import { db } from "@/utils/firebase";
-// import { getDocs, collection, query, where } from "firebase/firestore";
-import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
 
 const SensorDataTable = () => {
   const [sensorData, setSensorData] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchSensorData = async () => {
-  //     try {
-  //       const mainCollectionRef = collection(db, "devices");
-
-  //       // Reference to a specific document in the main collection
-  //       const mainDocRef = doc(mainCollectionRef, "iot-project");
-
-  //       // Reference to the subcollection within the main document ("Sensors-data")
-  //       const sensorsDataCollectionRef = collection(mainDocRef, "Sensors-data");
-
-  //       // Reference to the documents within the "Sensors-data" subcollection
-  //       const sensorsDataDocs = await getDocs(sensorsDataCollectionRef);
-
-  //       // Iterate through each document in "Sensors-data" subcollection
-  //       sensorsDataDocs.forEach(async (sensorDoc) => {
-  //         // console.log("Sensor Document ID:", sensorDoc.id);
-
-  //         // Reference to subcollections within each sensor document ("temp", "humidity", "co2", "lux")
-  //         const tempCollectionRef = collection(sensorDoc.ref, "temp");
-  //         const humidityCollectionRef = collection(sensorDoc.ref, "humidity");
-  //         const co2CollectionRef = collection(sensorDoc.ref, "CO2");
-  //         const luxCollectionRef = collection(sensorDoc.ref, "lux");
-
-  //         // Fetch data from each subcollection
-  //         const tempDocs = await getDocs(tempCollectionRef);
-  //         const humidityDocs = await getDocs(humidityCollectionRef);
-  //         const co2Docs = await getDocs(co2CollectionRef);
-  //         const luxDocs = await getDocs(luxCollectionRef);
-
-  //         // Log data from each subcollection
-  //         tempDocs.forEach((tempDoc) => {
-  //           console.log("Temp Data:", tempDoc.data());
-  //         });
-
-  //         humidityDocs.forEach((humidityDoc) => {
-  //           console.log("Humidity Data:", humidityDoc.data());
-  //         });
-
-  //         co2Docs.forEach((co2Doc) => {
-  //           console.log("CO2 Data:", co2Doc.data());
-  //         });
-
-  //         luxDocs.forEach((luxDoc) => {
-  //           console.log("Lux Data:", luxDoc.data());
-  //         });
-
-  //         tempDocs.forEach((tempDoc) => {
-  //           data.push({ ...tempDoc.data() });
-  //         });
-
-  //         humidityDocs.forEach((humidityDoc) => {
-  //           data.push({ ...humidityDoc.data() });
-  //         });
-
-  //         co2Docs.forEach((co2Doc) => {
-  //           data.push({ ...co2Doc.data() });
-  //         });
-
-  //         luxDocs.forEach((luxDoc) => {
-  //           data.push({ ...luxDoc.data() });
-  //         });
-  //       });
-
-  //       // setSensorData(data);
-
-  //       setSensorData(data);
-  //     } catch (error) {
-  //       console.error("Error fetching sensor data:", error);
-  //     }
-  //   };
-
-  //   fetchSensorData();
-  // }, []);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Initialize Firebase
-        // const db = getFirestore();
-
-        // Reference to the "device1" collection
         const device1CollectionRef = collection(db, "device1");
-
-        // Fetch documents from the "device1" collection
         const device1Docs = await getDocs(device1CollectionRef);
 
-        // Extract data from each document and update the state
         const data = [];
-        // console.log(data);
         device1Docs.forEach((doc) => {
-          console.log(doc);
           const { CO2, humidity, lux, myTimestamp, temp } = doc.data();
           data.push({
-            timestamp: myTimestamp, // Assuming myTimestamp is the timestamp field
+            id: doc.id, // Adding document ID to use for delete operation
+            timestamp: myTimestamp,
             temperature: temp,
             humidity,
             co2: CO2,
@@ -109,7 +25,6 @@ const SensorDataTable = () => {
           });
         });
 
-        // Update the state with the fetched data
         setSensorData(data);
       } catch (error) {
         console.error("Error fetching sensor data:", error);
@@ -117,51 +32,73 @@ const SensorDataTable = () => {
     };
 
     fetchData();
-  }, []); // Empty dependency array to run the effect only once on mount
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      const device1DocRef = doc(db, "device1", id);
+      await deleteDoc(device1DocRef);
+
+      // Update the state after deletion
+      setSensorData((prevData) => prevData.filter((sensor) => sensor.id !== id));
+    } catch (error) {
+      console.error("Error deleting sensor data:", error);
+    }
+  };
+
   return (
     <div className="container mx-auto mt-8">
-    <h1 className="text-3xl font-bold mb-4 dark:text-white">Sensor Data Table</h1>
-    <div className="overflow-x-auto">
-      <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700">
-        <thead>
-          <tr className="bg-gray-100 dark:bg-gray-700">
-            <th className="py-2 px-4 border-b dark:border-gray-600">
-              Timestamp
-            </th>
-            <th className="py-2 px-4 border-b dark:border-gray-600">
-              Temperature
-            </th>
-            <th className="py-2 px-4 border-b dark:border-gray-600">
-              Humidity
-            </th>
-            <th className="py-2 px-4 border-b dark:border-gray-600">CO2</th>
-            <th className="py-2 px-4 border-b dark:border-gray-600">Lux</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sensorData.map((sensor, index) => (
-            <tr key={index}>
-              <td className="py-2 px-4 border-b dark:border-gray-600">
-                {sensor.timestamp}
-              </td>
-              <td className="py-2 px-4 border-b dark:border-gray-600">
-                {sensor.temperature}
-              </td>
-              <td className="py-2 px-4 border-b dark:border-gray-600">
-                {sensor.humidity}
-              </td>
-              <td className="py-2 px-4 border-b dark:border-gray-600">
-                {sensor.co2}
-              </td>
-              <td className="py-2 px-4 border-b dark:border-gray-600">
-                {sensor.lux}
-              </td>
+      <h1 className="text-3xl font-bold mb-4 dark:text-white">Sensor Data Table</h1>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700">
+          <thead>
+            <tr className="bg-gray-100 dark:bg-gray-700">
+              <th className="py-2 px-4 border-b dark:border-gray-600">
+                Timestamp
+              </th>
+              <th className="py-2 px-4 border-b dark:border-gray-600">
+                Temperature
+              </th>
+              <th className="py-2 px-4 border-b dark:border-gray-600">
+                Humidity
+              </th>
+              <th className="py-2 px-4 border-b dark:border-gray-600">CO2</th>
+              <th className="py-2 px-4 border-b dark:border-gray-600">Lux</th>
+              {/* <th className="py-2 px-4 border-b dark:border-gray-600">Actions</th> */}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {sensorData.map((sensor, index) => (
+              <tr key={index}>
+                <td className="py-2 px-4 border-b dark:border-gray-600">
+                  {sensor.timestamp}
+                </td>
+                <td className="py-2 px-4 border-b dark:border-gray-600">
+                  {sensor.temperature}
+                </td>
+                <td className="py-2 px-4 border-b dark:border-gray-600">
+                  {sensor.humidity}
+                </td>
+                <td className="py-2 px-4 border-b dark:border-gray-600">
+                  {sensor.co2}
+                </td>
+                <td className="py-2 px-4 border-b dark:border-gray-600">
+                  {sensor.lux}
+                </td>
+                {/* <td className="py-2 px-4 border-b dark:border-gray-600">
+                  <button
+                    onClick={() => handleDelete(sensor.id)}
+                    className="text-red-500 hover:text-red-700"
+                  >
+                    Delete
+                  </button>
+                </td> */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
   );
 };
 
--- app/components/sensorsData/index.js
+++ app/components/sensorsData/index.js
@@ -1,43 +1,46 @@
 // pages/dashboard.js
 
-// import { useEffect, useState } from "react";
 import React, { useState, useEffect } from "react";
-
 import { db } from "@/utils/firebase";
-// import db from "@/utils/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, query, orderBy, limit } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Sensor = () => {
-  const [sensorData, setSensorData] = useState([]);
   const [user, setUser] = useState(null);
+  const [sensorData, setSensorData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const collectionRef = collection(db, "devices");
-        const querySnapshot = await getDocs(collectionRef);
+  const fetchData = async () => {
+    try {
+      const device1CollectionRef = collection(db, "device1");
 
-        if (!querySnapshot.empty) {
-          const docsData = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
+      // Fetch only one document, ordered by timestamp in descending order
+      const device1Query = query(
+        device1CollectionRef,
+        orderBy("myTimestamp", "desc"), // Change to your timestamp field
+        limit(1)
+      );
 
-          // setData(docsData);
-          setSensorData(docsData);
+      const device1Docs = await getDocs(device1Query);
 
-          // console.log(docsData);
-        } else {
-          console.log("No documents found");
-        }
-      } catch (error) {
-        console.error("Error fetching documents:", error);
+      if (!device1Docs.empty) {
+        const docData = device1Docs.docs[0].data();
+        const { CO2, humidity, lux, myTimestamp, temp } = docData;
+
+        // Update state with the fetched data
+        setSensorData({
+          timestamp: myTimestamp,
+          temperature: temp,
+          humidity,
+          co2: CO2,
+          lux,
+        });
+      } else {
+        console.log("No documents found");
       }
-    };
-
-    fetchData();
-  }, []);
+    } catch (error) {
+      console.error("Error fetching sensor data:", error);
+    }
+  };
 
   useEffect(() => {
     const auth = getAuth();
@@ -48,48 +51,75 @@ const Sensor = () => {
     return () => unsubscribe();
   }, []);
 
+  // Fetch fresh data on component mount
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h2 className="text-xl font-medium m-4 ">
-        {" "}
-        Welcome back, {user ? user.displayName || user.email : "UserName"} 🙂
-      </h2>
-      <h2 className="text-xl font-medium m-4 ">📊 Dashboard</h2>
+      {/* Check if sensorData is not null before rendering */}
+      {sensorData && (
+        <>
+          <h2 className="text-xl font-medium m-4 ">
+            Welcome back, {user ? user.displayName || user.email : "UserName"}{" "}
+            🙂
+          </h2>
+          <h2 className="text-xl font-medium m-4 ">📊 Dashboard</h2>
 
-      <div className="grid grid-cols-2 xl:grid-cols-4 gap-4 mb-4">
-        <SensorBox
-          title="Temperature"
-          unit="°C"
-          data={sensorData.map((reading) => reading.Temperature)}
-        />
-        <SensorBox
-          title="Humidity"
-          unit="%"
-          data={sensorData.map((reading) => reading.Humidity)}
-        />
-        <SensorBox
-          title="CO2"
-          unit="PPM"
-          data={sensorData.map((reading) => reading.CO2)}
-        />
-        <SensorBox
-          title="Lux"
-          unit="Lux"
-          data={sensorData.map((reading) => reading.Lux)}
-        />
-      </div>
+          <h3>Last Update : {sensorData.timestamp}</h3>
+
+          <div className="grid grid-cols-2 xl:grid-cols-4 gap-4 mb-4">
+            <SensorBox
+              title="Temperature"
+              unit="°C"
+              data={sensorData.temperature}
+              timestamp={sensorData.timestamp}
+            />
+            <SensorBox
+              title="Humidity"
+              unit="%"
+              data={sensorData.humidity}
+              timestamp={sensorData.timestamp}
+            />
+            <SensorBox
+              title="CO2"
+              unit="PPM"
+              data={sensorData.co2}
+              timestamp={sensorData.timestamp}
+            />
+            <SensorBox
+              title="Lux"
+              unit="Lux"
+              data={sensorData.lux}
+              timestamp={sensorData.timestamp}
+            />
+          </div>
+
+          {/* Button to fetch fresh data */}
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={fetchData}
+          >
+            Refresh Data
+          </button>
+        </>
+      )}
     </div>
   );
 };
 
-const SensorBox = ({ title, unit, data }) => (
+const SensorBox = ({ title, unit, data, timestamp }) => (
   <div className="h-32 md:h-28 lg:h-32 dark:bg-gray-800 bg-white border rounded-lg p-4 shadow-md">
     <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold">
       {title}
     </h2>
     <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-xl">
-      {data.map((value) => `${value} ${unit}`).join(", ")}
+      {`${title}: ${data} ${unit}`}
     </p>
+    {/* <p className="mt-2 text-sm sm:text-base md:text-lg lg:text-xl">
+      {`Timestamp: ${timestamp}`}
+    </p> */}
   </div>
 );
 
