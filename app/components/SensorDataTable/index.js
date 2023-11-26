fa964cc114cfd64b5cb73b2e8764e3392d08c9f1
Saipreetham <39019648+Saipreetham0@users.noreply.github.com>
2023-11-26 13:02:09 +0530

    Charts Update

--- app/components/SensorDataTable/index.js
+++ app/components/SensorDataTable/index.js
@@ -1,20 +1,68 @@
 // SensorDataTable.js
 import React, { useState, useEffect } from "react";
 import { db } from "@/utils/firebase";
-import { collection, getDocs, deleteDoc, doc, orderBy, query, startAfter, limit } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  orderBy,
+  query,
+  startAfter,
+  limit,
+} from "firebase/firestore";
+
+import { Line } from "react-chartjs-2";
+import "chart.js"; // Ensure Chart.js is imported
+
+import LineChart from "../chart";
 
 const SensorDataTable = () => {
   const [sensorData, setSensorData] = useState([]);
   const [lastDocument, setLastDocument] = useState(null);
 
+  // const chartData = {
+  //   labels: sensorData.map((sensor) => sensor.timestamp),
+  //   datasets: [
+  //     {
+  //       label: "Temperature",
+  //       data: sensorData.map((sensor) => sensor.temperature),
+  //       borderColor: "rgba(255, 99, 132, 1)",
+  //       borderWidth: 1,
+  //       fill: false,
+  //     },
+  //     // Add similar datasets for other sensor data (humidity, CO2, lux)
+  //   ],
+  // };
+
+  // const chartOptions = {
+  //   scales: {
+  //     x: {
+  //       type: "time",
+  //       position: "bottom",
+  //       time: {
+  //         unit: "minute",
+  //       },
+  //     },
+  //   },
+  // };
+
   const fetchNextPage = async () => {
     try {
       const device1CollectionRef = collection(db, "device1");
 
-      // If there is a lastDocument, start the query after it
       const q = lastDocument
-        ? query(device1CollectionRef, orderBy("myTimestamp", "desc"), startAfter(lastDocument), limit(10))
-        : query(device1CollectionRef, orderBy("myTimestamp", "desc"), limit(10));
+        ? query(
+            device1CollectionRef,
+            orderBy("myTimestamp", "desc"),
+            startAfter(lastDocument),
+            limit(10)
+          )
+        : query(
+            device1CollectionRef,
+            orderBy("myTimestamp", "desc"),
+            limit(10)
+          );
 
       const device1Docs = await getDocs(q);
 
@@ -31,10 +79,7 @@ const SensorDataTable = () => {
         });
       });
 
-      // Update the lastDocument for the next page
       setLastDocument(device1Docs.docs[data.length - 1] || null);
-
-      // Update the state with the fetched data
       setSensorData((prevData) => [...prevData, ...data]);
     } catch (error) {
       console.error("Error fetching sensor data:", error);
@@ -42,7 +87,6 @@ const SensorDataTable = () => {
   };
 
   useEffect(() => {
-    // Fetch the first page on mount
     fetchNextPage();
   }, []);
 
@@ -50,74 +94,124 @@ const SensorDataTable = () => {
     try {
       const device1DocRef = doc(db, "device1", id);
       await deleteDoc(device1DocRef);
-
-      // Update the state after deletion
-      setSensorData((prevData) => prevData.filter((sensor) => sensor.id !== id));
+      setSensorData((prevData) =>
+        prevData.filter((sensor) => sensor.id !== id)
+      );
     } catch (error) {
       console.error("Error deleting sensor data:", error);
     }
   };
 
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-3xl font-bold mb-4 dark:text-white">
-        Sensor Data Table
-      </h1>
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700">
-          <thead>
-            <tr className="bg-gray-100 dark:bg-gray-700">
-              <th className="py-2 px-4 border-b dark:border-gray-600">
-                Timestamp
-              </th>
-              <th className="py-2 px-4 border-b dark:border-gray-600">
-                Temperature
-              </th>
-              <th className="py-2 px-4 border-b dark:border-gray-600">
-                Humidity
-              </th>
-              <th className="py-2 px-4 border-b dark:border-gray-600">CO2</th>
-              <th className="py-2 px-4 border-b dark:border-gray-600">Lux</th>
-              {/* <th className="py-2 px-4 border-b dark:border-gray-600">Actions</th> */}
-            </tr>
-          </thead>
-          <tbody>
-            {sensorData.map((sensor, index) => (
-              <tr key={index}>
-                <td className="py-2 px-4 border-b dark:border-gray-600">
-                  {sensor.timestamp}
-                </td>
-                <td className="py-2 px-4 border-b dark:border-gray-600">
-                  {sensor.temperature}
-                </td>
-                <td className="py-2 px-4 border-b dark:border-gray-600">
-                  {sensor.humidity}
-                </td>
-                <td className="py-2 px-4 border-b dark:border-gray-600">
-                  {sensor.co2}
-                </td>
-                <td className="py-2 px-4 border-b dark:border-gray-600">
-                  {sensor.lux}
-                </td>
-                {/* <td className="py-2 px-4 border-b dark:border-gray-600">
-                  <button
-                    onClick={() => handleDelete(sensor.id)}
-                    className="text-red-500 hover:text-red-700"
-                  >
-                    Delete
-                  </button>
-                </td> */}
+    <div>
+      {/* <LineChart  />
+       */}
+      <div className="grid grid-cols-1 xl:grid-cols-2 mt-5 gap-2">
+        {/* Add a LineChart component for each sensor */}
+        <LineChart
+          title="Temperature"
+          sensorData={sensorData.map((dataPoint) => ({
+            data: dataPoint.temperature,
+            timestamp: dataPoint.timestamp,
+          }))}
+          lineColor="#e74c3c"
+        />
+        <LineChart
+          title="Humidity"
+          sensorData={sensorData.map((dataPoint) => ({
+            data: dataPoint.humidity,
+            timestamp: dataPoint.timestamp,
+          }))}
+          lineColor="#2ecc71"
+        />
+        <LineChart
+          title="CO2"
+          sensorData={sensorData.map((dataPoint) => ({
+            data: dataPoint.co2,
+            timestamp: dataPoint.timestamp,
+          }))}
+          lineColor="#7f8c8d"
+        />
+        <LineChart
+          title="Lux"
+          sensorData={sensorData.map((dataPoint) => ({
+            data: dataPoint.lux,
+            timestamp: dataPoint.timestamp,
+          }))}
+          lineColor="#e67e22"
+        />
+      </div>
+
+      <div className="container mx-auto mt-8">
+        <h1 className="text-3xl font-bold mb-4 text-gray-800 dark:text-white">
+          Sensor Data Table
+        </h1>
+
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700">
+            <thead>
+              <tr className="bg-gray-100 dark:bg-gray-700">
+                <th className="py-2 px-4 border-b dark:border-gray-600">
+                  Timestamp
+                </th>
+                <th className="py-2 px-4 border-b dark:border-gray-600">
+                  Temperature
+                </th>
+                <th className="py-2 px-4 border-b dark:border-gray-600">
+                  Humidity
+                </th>
+                <th className="py-2 px-4 border-b dark:border-gray-600">CO2</th>
+                <th className="py-2 px-4 border-b dark:border-gray-600">Lux</th>
+                <th className="py-2 px-4 border-b dark:border-gray-600">
+                  Actions
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="mt-4">
-          <button
-            onClick={fetchNextPage}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Load More
-          </button>
+            </thead>
+            <tbody>
+              {sensorData.map((sensor, index) => (
+                <tr
+                  key={index}
+                  className={
+                    index % 2 === 0
+                      ? "bg-gray-50 dark:bg-gray-800"
+                      : "bg-white dark:bg-gray-700"
+                  }
+                >
+                  <td className="py-2 px-4 border-b dark:border-gray-600">
+                    {sensor.timestamp}
+                  </td>
+                  <td className="py-2 px-4 border-b dark:border-gray-600">
+                    {sensor.temperature}
+                  </td>
+                  <td className="py-2 px-4 border-b dark:border-gray-600">
+                    {sensor.humidity}
+                  </td>
+                  <td className="py-2 px-4 border-b dark:border-gray-600">
+                    {sensor.co2}
+                  </td>
+                  <td className="py-2 px-4 border-b dark:border-gray-600">
+                    {sensor.lux}
+                  </td>
+                  <td className="py-2 px-4 border-b dark:border-gray-600">
+                    <button
+                      onClick={() => handleDelete(sensor.id)}
+                      className="text-red-500 hover:text-red-700"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="mt-4">
+            <button
+              onClick={fetchNextPage}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Load More
+            </button>
+          </div>
         </div>
       </div>
     </div>
